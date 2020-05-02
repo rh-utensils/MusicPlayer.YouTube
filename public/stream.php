9063a44c13797a8820cdb4e4ff68a98a68f5b195
Rene Hampölz <service@hampoelz.net>
2020-05-02 15:10:02 +0200

    Change "Access-Control-Allow-Origin" header

--- public/stream.php
+++ public/stream.php
@@ -1,8 +1,8 @@
 <?php
 
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-    //header("Access-Control-Allow-Origin: https://music.hampoelz.net");
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: https://music.hampoelz.net");
+    //header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: GET"); 
 }  
 
